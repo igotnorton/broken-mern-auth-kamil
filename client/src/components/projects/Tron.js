5c845eb837a3a01abb162427633d81d0a98f6192
igotnorton <kamiloleskiewicz@gmail.com>
2022-11-02 14:37:20 +0000

    new file:   client/src/components/projects/Tron.js
     Changes not staged for commit:
    	modified:   client/build/asset-manifest.json
    	modified:   client/build/index.html
    	modified:   client/build/materialize.min.css
    	deleted:    client/build/precache-manifest.b81656e34b58e87529ff174360721825.js
    	modified:   client/build/service-worker.js
    	deleted:    client/build/static/js/main.b11dd097.chunk.js
    	deleted:    client/build/static/js/main.b11dd097.chunk.js.map
    	modified:   client/public/index.html
    	modified:   client/public/materialize.min.css
    	modified:   client/src/App.js
    	modified:   client/src/actions/authActions.js
    	modified:   client/src/components/auth/Register.js
    	modified:   client/src/components/dashboard/Dashboard.js
    	modified:   client/src/components/layout/Landing.js
    	modified:   config/keys.js
    	modified:   package.json
    
     Untracked files:
    	client/build/precache-manifest.4ccbf7a6cf6fd5b1c686e16a2c78afd5.js
    	client/build/static/js/main.f68a012d.chunk.js
    	client/build/static/js/main.f68a012d.chunk.js.map
    	client/public/tron/
    	client/src/components/projects/materialize.min.css

---
+++ client/src/components/projects/Tron.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+// import { Link } from "react-router-dom";
+
+
+class Tron extends Component {
+  render() {
+    return (
+        <iframe id="magicFrame" src="./tron/index.html"></iframe>
+    );
+  }
+}
+
+
+export default Tron;
